55c93653a07e6c69591fe8fff998bbc835038fd7
jecko78 <jecko78@gmail.com>
2024-01-22 16:51:45 +0700

    Complete build V1

--- public/script.js
+++ public/script.js
@@ -1,20 +1,39 @@
 class Player {
   constructor(game){
     this.game = game;
-    this.width =200;
-    this.height = 100;
+    this.width =140;
+    this.height = 120;
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
     this.speed = 10;
     this.lives = 3;
+    this.image = document.getElementById('player');
+    this.jets_image = document.getElementById('player_jets');
+    this.frameX = 0;
+    this.jetsFrame = 1;
   }
   draw(context){
-    context.fillRect(this.x, this.y, this.width, this.height);
+    //context.fillRect(this.x, this.y, this.width, this.height);
+    //handle sprite frames
+    if (this.game.keys.indexOf('1') > -1){
+      this.frameX = 3;
+    } else {
+      this.frameX = 0;
+    }
+    context.drawImage(this.jets_image, this.jetsFrame * this.width, 0, this.width, this.height, this.x, this.y,this.width, this.height);
+    context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y,this.width, this.height);
   }
   update(){
   //horizontal movement
-    if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
-  if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+    if (this.game.keys.indexOf('ArrowLeft') > -1) {
+      this.x -= this.speed;
+      this.jetsFrame = 0;
+    } else if (this.game.keys.indexOf('ArrowRight') > -1) {
+       this.x += this.speed;
+       this.jetsFrame = 2;
+    } else {
+      this.jetsFrame = 1;
+    }
   //horizontal boundaries
   if (this.x < -this.width * 0.5) this.x = -this.width * 0.5;
   else if (this.x > this.game.width - this.width * 0.5) this.x = this.game.width - this.width * 0.5;
@@ -32,7 +51,7 @@ class Player {
 
 class Projectile {
   constructor(){
-    this.width = 15;
+    this.width = 5;
     this.height = 20;
     this.x = 0;
     this.y = 0;
@@ -41,7 +60,10 @@ class Projectile {
   }
   draw(context){
     if (!this.free){
+      context.save();
+      context.fillStyle = 'gold';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.restore();
     }
   }
   update(){
